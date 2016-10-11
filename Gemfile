1166bc379aa44cdb4d289225408f69b2d675279e
laura <lauragamarra64@gmail.com>
2016-10-11 19:34:00 -0300

    agregue gemas cambie home y otros funciona todo

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'devise'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'simple_form'
 gem 'devise-bootstrap-views'
+gem 'bootstrap-navbar', '~> 2.3'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,37 @@ class HomeController < ApplicationController
   def index
   	@productos = Producto.all
   end
+def show
+	@presupuesto = Presupuesto.includes(:presupuesto_detalles).find(params[:id])
+	
 end
+
+end
+
+
+#<%= link_to 'Ver Presupuesto' home_path(@presupuesto)
+# show.html.erb
+#<h1> Mostrando presupuesto  </h1>
+#<p> Fecha: <%= @presupuesto.fecha %> </p>
+#<table>
+#	<thead>
+#		<tr>
+#			<th>Producto</th>
+#			<th>Imagen</th>
+#			<th>Cantidad</th>
+##			<th>Precio Unitario</th>
+#			<th>Subtotal</th>
+#		</tr>
+#	</thead>
+#	</tbody>	
+#		<% @presupuesto.presupuesto_detalles.each do|detalle| %>  
+#		<tr>
+##
+#			<td></td>
+#			<td></td>
+#			<td></td>
+#			<td></td>
+#			<td></td>
+#		</tr>
+#	</tbody>
+#</table>
