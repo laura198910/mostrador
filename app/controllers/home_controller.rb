c9f418698638dd846378eb8ae177a6a88943fe82
patricia <patycaballero.1995@gmail.com>
2016-12-13 19:14:37 -0300

    agregue vista de agregar carrito

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,36 +7,103 @@ def show
 	
 end
 def add
+	if cookies[:presupuesto_id]
+       presupuesto_id = cookies[:presupuesto_id]# La cookies guarda el presupuesto id dentro de si mismo 
+       @presupuesto = Presupuesto.find(presupuesto_id)
+    else
+       @presupuesto = Presupuesto.new(fecha: DateTime.now)
+    end
+    puts params.inspect
+    producto = Producto.find(params[:id])
+    detalle =  @presupuesto.presupuesto_detalles.where(producto_id: producto.id ).first
+    if detalle
+      detalle.cantidad +=1
+    else
+      @presupuesto.presupuesto_detalles.build(cantidad: 1, producto_id: producto.id)
+    end
+     
+      puts detalle.inspect
+      puts @presupuesto.inspect
 
-	#cookies.delete :presupuesto_detalle_id
+    detalle.save if detalle
+    if @presupuesto.save
+      # setear la cookies
+      cookies[:presupuesto_id] = @presupuesto.id
+      msg = "Se ha agregado #{producto.nombre} correctamente "
+      flash[:notice] =  msg
+      #flash.now  Esto se usa para render 
+    else
+      msg = "No se ha guardado #{producto.nombre} correctamente "
+      flash[:error] =  msg
+    end
+      #redirect_to presupuesto_path(@presupuesto)
+      redirect_to home_index_path
+ end
 
-	if cookies[:presupuesto_detalle_id]
-		@presupuesto = Presupuesto.find ( cookies[:presupuesto_detalle_id])
-	else
-		@presupuesto = Presupuesto.new(fecha: Date.today)
-	end
+  def sumarProducto
+    #cookies.delete :presupuesto_id
+    if cookies[:presupuesto_id]
+       presupuesto_id = cookies[:presupuesto_id]# La cookies guarda el presupuesto id dentro de si mismo 
+       @presupuesto = Presupuesto.find(presupuesto_id)
+    else
+       @presupuesto = Presupuesto.new(fecha: DateTime.now)
+    end
+    puts params.inspect
+    producto = Producto.find(params[:id])
+    detalle =  @presupuesto.presupuesto_detalles.where(producto_id: producto.id ).first
+    if detalle
+      detalle.cantidad +=1
+    else
+      @presupuesto.presupuesto_detalles.build(cantidad: 1, producto_id: producto.id)
+    end
+     
+      puts detalle.inspect
+      puts @presupuesto.inspect
 
-	puts params.inspect
-	producto = Producto.find(params[:id])
+    detalle.save if detalle
+    if @presupuesto.save
+      # setear la cookies
+      cookies[:presupuesto_id] = @presupuesto.id
+      msg = "Se ha agregado #{producto.nombre} correctamente "
+      flash[:notice] =  msg
+      #flash.now  Esto se usa para render 
+    else
+      msg = "No se ha guardado #{producto.nombre} correctamente "
+      flash[:error] =  msg
+    end
+      #redirect_to presupuesto_path(@presupuesto)
+      redirect_to home_show_path
+ end
+  def sumar_carrito
+  @sumar =  2
 
-	detalle = @presupuesto.presupuesto_detalles.where(producto_id: producto.id).first
-	
-	if detalle
-		detalle.cantidad +=1
-	else
-		detalle = @presupuesto.presupuesto_detalles.build(cantidad: 1, producto_id: producto.id)
-	end
+  end
 
+def eliminar
+   presupuesto_id = cookies[:presupuesto_id]
+    @presupuesto = Presupuesto.find(presupuesto_id)
 
-	if @presupuesto.save
-		cookies[:presupuesto_detalle_id] = @presupuesto.id
-	else
-
-	end
-
-	redirect_to root_path
-
+    producto = Producto.find(params[:id])
+    detalle =  @presupuesto.presupuesto_detalles.where(producto_id: producto.id ).first
+    if !detalle
+      cookies.delete :presupuesto_id
+    else
+      detalle.cantidad -=  1
+       #.destroy
+    end
+    if @presupuesto.save
+       detalle.save
+      msg = "Se ha eliminado #{producto.nombre} correctamente "
+      flash[:notice] =  msg
+      #flash.now  Esto se usa para render 
+    else
+      msg = "No se ha eliminado #{producto.nombre} correctamente "
+      flash[:error] =  msg
+    end
+      #redirect_to presupuesto_path(@presupuesto)
+      redirect_to home_show_path
 end
+
 end
 
 #<%= link_to 'Ver Presupuesto' home_path(@presupuesto)
